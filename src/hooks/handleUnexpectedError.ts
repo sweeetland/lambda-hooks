46d0130de4e03bf4991e727488ef1ba04cea5c9f
James Sweetland <jamesadamsweetland@gmail.com>
2020-11-05 12:03:01 +0200

    add logging to handleUnexpectedError

--- src/hooks/handleUnexpectedError.ts
+++ src/hooks/handleUnexpectedError.ts
@@ -3,6 +3,8 @@ import { Hook } from '../index'
 export const handleUnexpectedError: Hook = async state => {
     const { error } = state
 
+    console.log('unexpected error: ', error)
+
     state.exit = true
 
     state.response = {
